1e3aed4a822cf6fa024bf095c361579a42f31dfa
Jay Phan <kha21052004@gmail.com>
2022-05-21 16:27:21 -0300

    Title

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>hello</div>
+    <div className="Title">
+      
+      
+      <div>
+        <img className = "title_background" src="https://images.dailyhive.com/20211124132001/sd.jpeg"></img>
+      </div>
+
+      <h1 className = "title"> Gas Price Tracker</h1>
+
+      
     </div>
+
+
   );
 }
 
